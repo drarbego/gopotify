935c45f3e166171a036cfd0fe25b568e224d17fa
Edgar A. García <drarbego@gmail.com>
2023-02-12 02:52:30 -0600

    update test scene

--- test.gd
+++ test.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 
 func _on_Play_pressed():
-	$Spotidot.play()
+	$Gopotify.play()
 
 func _on_Pause_pressed():
-	$Spotidot.pause()
+	$Gopotify.pause()
 
 func _on_RefreshToken_pressed():
-	$Spotidot.request_user_authorization()
+	$Gopotify.request_user_authorization()
